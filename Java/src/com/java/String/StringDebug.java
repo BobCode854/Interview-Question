aca81f0aef31b096439852db1c7dda3021360dee
KASINHA <karan.sinha@mercedes-benz.com>
2024-01-11 14:26:08 +0530

    Pushing java changes

---
+++ Java/src/com/java/String/StringDebug.java
@@ -0,0 +1,36 @@
+package com.java.String;
+
+
+/**
+ *                                      String
+ *                ->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *                  **) Sequence of character
+ *                  
+ *                  How To Represent String in Java
+ *                ------------------------------------------
+ *                  There are 4 ways .
+ *                  1) String (class) present in java.lang package
+ *                  2) String Buffer
+ *                  3) String Builder
+ *                  4) Array
+ *                  
+ *                 
+ *                 How To Create String Object
+ *                 --------------------------------------------
+ *                 There are 2 ways.
+ *                 1) using new keyword
+ *                 2) using String literals
+ *                 
+ *                 String s1="Karan";
+ *                 String s2 = new String("Karan");
+ *                  
+ *                  
+ */
+public class StringDebug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
